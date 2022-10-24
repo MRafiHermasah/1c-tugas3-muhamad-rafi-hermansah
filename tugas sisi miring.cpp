b82eb3dea4a56a67cd2d339ba57bcbd14fa08817
Muhamad Rafi Hermansah <114910465+MRafiHermasah@users.noreply.github.com>
2022-10-24 23:58:23 +0700

    Add files via upload

---
+++ tugas sisi miring.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float AC;
+    cout<<"masukan nilai sisi alas = 8\n";
+  
+    cout<<"masukan nilai sisi tinggi = 10\n";
+    
+    AC=(8*8)+(10*10);
+    AC=sqrt(AC);
+    cout<<"sisi miring segitiga = "<<AC<<endl;
+    return 0;
+} 
